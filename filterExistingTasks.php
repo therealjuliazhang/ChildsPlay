b9c9955139cfb3fd443cc638511232f092d52479
ExtremeRen <52590215+ExtremeRen@users.noreply.github.com>
2019-09-21 15:02:37 +1000

    design updtae

--- filterExistingTasks.php
+++ filterExistingTasks.php
@@ -31,18 +31,12 @@
   <div class="container">
     <h4 class="blue-text darken-2">Filter By:</h4>
     <h5>Date Created</h5>
-    <table>
-      <tr>
-        <td>
-          Start<br/>
-          <input type="text" class="datepicker" id="startDate">
-        </td>
-        <td>
-          End<br/>
-          <input type="text" class="datepicker" id="endDate">
-        </td>
-      </tr>
-    </table>
+    <div class="row">
+      <div class="col s6">Start</div>
+      <div class="col s6">End</div>
+      <div class="col s6"><input type="text" class="datepicker" id="startDate"></div>
+      <div class="col s6"><input type="text" class="datepicker" id="endDate"></div>
+    </div>
 
     <h5>Task Type</h5>
     <div class="row">
@@ -56,50 +50,42 @@
         </select>
       </div>
     </div>
-    <!--
-    <ul id = "dropdown" class = "dropdown-content">
-    <li><a href = "#">Identify Body Parts</a></li>
-    <li><a href = "#">Character Ranking</a></li>
-    <li><a href = "#">Likert Scale</a></li>
-    <li><a href = "#">Preferred Mechanics</a></li>
-    <a class = "btn dropdown-button blue darken-4" href = "#" data-activates = "dropdown">Task Type</a>
-  </ul>
--->
-<!--table for holding tasks-->
-<table class="striped">
-  <thead>
-    <tr class="blue-text darken-2">
-      <th>TaskID</th>
-      <th>Instruction</th>
-      <th>Activity Style</th>
-      <th>Preview</th>
-      <th>Edit</th>
-      <th>Add</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-    include 'db_connection.php';
-    $conn = OpenCon();
-    $query = "SELECT * FROM TASK";
-    $result = $conn->query($query);
-    while($row = mysqli_fetch_assoc($result)){
-      echo "<tr><td>".$row["taskID"]."</td>".
-      "<td>".$row["instruction"]."</td>".
-      "<td>".$row["activityStyle"]."</td>".
-      "<td><a class='waves-effect waves-light btn blue darken-2' href='instruction.php?taskID=".$row["taskID"]."&mode=preview&from=existingTasks'>Preview</a></td>".
-      "<td><a class='waves-effect waves-light btn blue darken-4' href='CreateNewTaskInCreateTest.php?exist=true&taskID=".$row["taskID"]."'>Edit</a></td>".
-      "<td><a class='waves-effect waves-light btn blue darken-4' href='createTest.php?taskID=".$row["taskID"]."'>Add</a></td>";
-    }
-    //CloseCon($conn);
-    ?>
-  </tbody>
-</table>
-<div class="row">
-  <div class="col s1 offset-s11"><a class="waves-effect waves-light btn #2196f3 blue right" id="cancelButton">Cancel</a></div>
-</div>
-</div>
-<!--end body content-->
+
+    <!--table for holding tasks-->
+    <table class="striped">
+      <thead>
+        <tr class="blue-text darken-2">
+          <th class='taskIdCol'>TaskID</th>
+          <th class='indtructionCol'>Instruction</th>
+          <th class='activityStyleCol'>Activity Style</th>
+          <th class='previewCol'>Preview</th>
+          <th class='editCol'>Edit</th>
+          <th class='addCol'>Add</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        include 'db_connection.php';
+        $conn = OpenCon();
+        $query = "SELECT * FROM TASK";
+        $result = $conn->query($query);
+        while($row = mysqli_fetch_assoc($result)){
+          echo "<tr><td class='taskIdCol'>".$row["taskID"]."</td>".
+          "<td class='indtructionCol'>".$row["instruction"]."</td>".
+          "<td class='activityStyleCol'>".$row["activityStyle"]."</td>".
+          "<td class='previewCol'><a class='waves-effect waves-light btn blue darken-2' href='instruction.php?taskID=".$row["taskID"]."&mode=preview&from=existingTasks'>Preview</a></td>".
+          "<td class='editCol'><a class='waves-effect waves-light btn blue darken-4' href='CreateNewTaskInCreateTest.php?exist=true&taskID=".$row["taskID"]."'>Edit</a></td>".
+          "<td class='addCol'><a class='waves-effect waves-light btn blue darken-4' href='createTest.php?taskID=".$row["taskID"]."'>Add</a></td>";
+        }
+        //CloseCon($conn);
+        ?>
+      </tbody>
+    </table>
+    <div class="row">
+      <div class="col s1 offset-s11"><a class="waves-effect waves-light btn #2196f3 blue right" id="cancelButton">Cancel</a></div>
+    </div>
+  </div>
+  <!--end body content-->
 </body>
 <script>
 //Initialize Selector
@@ -118,12 +104,12 @@ $(function() {
   $("#startDate").datepicker();
   $("#endDate").datepicker();
 
-    $("#endDate").on("change",function(){
-      checkDate();
-    });
-    $("#startDate").on("change",function(){
-      checkDate();
-    });
+  $("#endDate").on("change",function(){
+    checkDate();
+  });
+  $("#startDate").on("change",function(){
+    checkDate();
+  });
 });
 function checkDate(){
   var startSelected = $('#startDate').val();
@@ -136,29 +122,7 @@ function checkDate(){
     alert('End date should be greater than Start date. Please select a valid range.');
   }
 }
-//Date validation
-/*
-$(function() {
-  $("#startDate").datepicker();
-  $("#startDate").on("change",function(){
-    var startSelected = $(this).val();
-    $("#endDate").datepicker();
-    $("#endDate").on("change",function(){
-      var endSelected = $(this).val();
-      //alert(startSelected + endSelected);
-      if(startSelected > endSelected){
-        alert('End date should be greater than Start date. Please select a valid range.');
-      }
-    });
-  });
-});
-*/
-/*
-$('.datepicker').pickadate({
-selectMonths: true, // Enable Month Selection
-selectYears: 10 // Creates a dropdown of 10 years to control year
-});
-*/
+
 </script>
 <style>
 #body {
@@ -196,5 +160,47 @@ selectYears: 10 // Creates a dropdown of 10 years to control year
 .datepicker-table td.is-selected {
   background-color: #1976D2;
 }
+tbody {
+  display:block;
+  height:300px;
+  overflow:auto;
+}
+thead, tbody tr {
+  display:table;
+  width:100%;
+  table-layout:fixed;
+}
+thead {
+  width: calc( 100% - 1em )
+}
+table {
+  width:100%;
+}
+th{
+  text-align: center;
+}
+.taskIdCol, .activityStyleCol, .previewCol, .editCol, .addCol{
+  text-align: center;
+}
+.taskIdCol{
+  width: 5%;
+}
+.instructionCol{
+  width: 40%;
+}
+.activityStyleCol{
+  width: 10%;
+}
+.previewCol{
+  width: 13%;
+}
+.editCol{
+  width: 13%;
+}
+.addCol{
+  width: 13%;
+}
+
+
 </style>
 </html>
