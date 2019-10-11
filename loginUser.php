f9f7ac7ed98fae2c83d0c0531dafbc9b23c431c3
Linh Bui <tiara4798@gmail.com>
2019-10-12 09:05:20 +1100

    minor change

--- loginUser.php
+++ loginUser.php
@@ -12,19 +12,21 @@
     $query = "SELECT userID, accountType, accepted FROM USERS WHERE username='$username' AND password='$password'";
   	$results = mysqli_query($conn, $query);
   	if (mysqli_num_rows($results) == 1) {
-        $user = mysqli_fetch_assoc($results);
-        //login fail if user not accepted by admin yet
-        if($user['accepted']==0)
-            header('location: login.php?msg=notaccepted');
-        //set user ID to session
-        $userID = $user['userID'];
-        $_SESSION['userID'] = $userID;
-        //check if admin or educator and redirect to correct page
-        if($user['accountType']==1)
-            header('location: viewExistingTests.php');
-        else
-  	        header('location: educatorTests.php');
+            $user = mysqli_fetch_assoc($results);
+            //login fail if user not accepted by admin yet
+            if($user['accepted']== 0)
+                header('location: login.php?msg=notaccepted');
+            else if ($user['accepted'] == 1){
+				//set user ID to session
+				$userID = $user['userID'];
+				$_SESSION['userID'] = $userID;
+			}
+            //check if admin or educator and redirect to correct page
+            if($user['accountType']== 1)
+                header('location: viewExistingTests.php');
+            else
+                header('location: educatorTests.php');
   	}else {
-        header('location: login.php?msg=failed');
+            header('location: login.php?msg=failed');
   	}
 ?>
