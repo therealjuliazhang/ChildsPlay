7d1bc381f8a116acc9656a60095f659d3d43e404
ExtremeRen <52590215+ExtremeRen@users.noreply.github.com>
2019-10-17 17:31:57 +1100

    Update design
    Card for datepicker, buttons etc

--- filterExistingTasks.php
+++ filterExistingTasks.php
@@ -5,11 +5,11 @@ Author:Phuong Linh Bui (5624095), Alex Satoru Hanrahan (4836789), Ren Sugie(5679
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Child'sPlay</title>
+  <title>Filter Existing Tasks</title>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <!-- CSS  -->
-  <link rel="stylesheet" type="text/css" href="childsPlayStyle.css">
+  <!-- <link rel="stylesheet" type="text/css" href="childsPlayStyle.css"> -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   <!--  Scripts-->
@@ -20,22 +20,22 @@ Author:Phuong Linh Bui (5624095), Alex Satoru Hanrahan (4836789), Ren Sugie(5679
 <script>
 //save filtering values when reload the page
 $(document).ready(function(){
-	var start = sessionStorage.getItem("start");
-	var end = sessionStorage.getItem("end");
-	var activityStyle = sessionStorage.getItem("activityStyle");
-	$("#startDate").val(start);
-	$("#endDate").val(end);
-	$("#activityStyle").val(activityStyle);
-	sessionStorage.clear();
+  var start = sessionStorage.getItem("start");
+  var end = sessionStorage.getItem("end");
+  var activityStyle = sessionStorage.getItem("activityStyle");
+  $("#startDate").val(start);
+  $("#endDate").val(end);
+  $("#activityStyle").val(activityStyle);
+  sessionStorage.clear();
 });
 
 function filter(){
-	var start = $("#startDate").val();
-	var end = $("#endDate").val();
-	var activityStyle = $("#activityStyle option:selected").val();
-	sessionStorage.setItem("start", start);
-	sessionStorage.setItem("end", end);
-	sessionStorage.setItem("activityStyle", activityStyle);
+  var start = $("#startDate").val();
+  var end = $("#endDate").val();
+  var activityStyle = $("#activityStyle option:selected").val();
+  sessionStorage.setItem("start", start);
+  sessionStorage.setItem("end", end);
+  sessionStorage.setItem("activityStyle", activityStyle);
 }
 </script>
 
@@ -53,82 +53,105 @@ function filter(){
   <!--end header-->
   <!-- body content -->
   <?php
-	session_start();
-	if (isset($_SESSION['userID']))
-		$userID = $_SESSION['userID'];
-	else
-		header('location: login.php');
+  session_start();
+  if (isset($_SESSION['userID']))
+  $userID = $_SESSION['userID'];
+  else
+  header('location: login.php');
   ?>
   <div class="container">
-    <h5 class="blue-text darken-2">Filter By:</h5>
-    <h6 class="blue-text darken-2 header">Date Created</h6>
-	<form action="" method="post">
-    <div class="row">
-      <div class="col s6">Start date</div>
-      <div class="col s6">End date</div>
-      <div class="col s6"><input type="text" class="datepicker" id="startDate" name="startDate"></div>
-      <div class="col s6"><input type="text" class="datepicker" id="endDate" name="endDate"></div>
-    </div>
+    <h3 class="blue-text text-darken-4">Filter By:</h3>
 
-		<h6 class="blue-text darken-2 header">Activity Style</h6>
-		<div class="row">
-		  <div class="input-field col s6">
-			<select class="" name="activityStyle" id="activityStyle">
-			  <option value="" selected disabled>Select Activity Style:</option>
-			  <option value="Identify Body Parts">Identify Body Parts</option>
-			  <option value="Character Ranking">Character Ranking</option>
-			  <option value="Likert Scale">Likert Scale</option>
-			  <option value="Preferred Mechanics">Preferred Mechanics</option>
-			</select>
-		  </div>
-		</div>
-		<div class="row">
-			<button class="btn waves-effect waves-light blue darken-4 sortButton" type="submit" name="submitFilter" onclick="filter()">Filter</button>
-		</div>
-	</form>
-	<br/>
+    <div class="card-panel z-depth-3">
+      <div class="row">
+        <div class="col s4">
+          <h5 class="blue-text text-darken-4 header">Date Created</h5>
+        </div>
+        <div class="col s4 offset-s4">
+          <h5 class="blue-text text-darken-4 header">Activity Style</h5>
+        </div>
+        <form action="" method="post">
+          <div class="col s4">Start date</div>
+          <div class="col s4">End date</div>
+          <div class="col s4"></br></div>
+          <div class="col s4"><input type="text" class="datepicker" id="startDate" name="startDate"></div>
+          <div class="col s4"><input type="text" class="datepicker" id="endDate" name="endDate"></div>
+          <div class="input-field col s4">
+            <select class="" name="activityStyle" id="activityStyle">
+              <option value="" selected disabled>Select Activity Style:</option>
+              <option value="Identify Body Parts">Identify Body Parts</option>
+              <option value="Character Ranking">Character Ranking</option>
+              <option value="Likert Scale">Likert Scale</option>
+              <option value="Preferred Mechanics">Preferred Mechanics</option>
+            </select>
+          </div>
+          <div class="col s12">
+            <br>
+            <button class="btn waves-effect waves-light blue darken-4 sortButton" type="submit" name="submitFilter" onclick="filter()">Filter</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <!--
+    <h6 class="blue-text text-darken-4 header">Activity Style</h6>
+    <div class="row">
+    <div class="input-field col s6">
+    <select class="" name="activityStyle" id="activityStyle">
+    <option value="" selected disabled>Select Activity Style:</option>
+    <option value="Identify Body Parts">Identify Body Parts</option>
+    <option value="Character Ranking">Character Ranking</option>
+    <option value="Likert Scale">Likert Scale</option>
+    <option value="Preferred Mechanics">Preferred Mechanics</option>
+  </select>
+</div>
+</div>
+<div class="row">
+<button class="btn waves-effect waves-light blue darken-4 sortButton" type="submit" name="submitFilter" onclick="filter()">Filter</button>
+</div>
+</form> -->
+<br/>
 <!--table for holding tasks-->
 <div style="overflow:auto">
-<table class="striped">
-  <thead>
-    <tr class="blue-text darken-2">
-      <th class='taskIdCol'>Title</th>
-      <th class='instructionCol'>Instruction</th>
-      <th class='activityStyleCol'>Activity Style</th>
-      <th class='dateCreatedCol'>Date Created</th>
-      <!-- <th class='previewCol'>Preview</th>
-      <th class='editCol'>Edit</th>
-      <th class='addCol'>Add</th> -->
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-	//unset the sessions for preview tasks in filter existing tasks from comments.php
-	if(isset($_SESSION["create"]))
-		unset($_SESSION["create"]);
-	if(isset($_SESSION["edit"]))
-		unset($_SESSION["edit"]);
+  <table class="striped">
+    <thead>
+      <tr class="blue-text text-darken-4">
+        <th class='taskIdCol'>Title</th>
+        <th class='instructionCol'>Instruction</th>
+        <th class='activityStyleCol'>Activity Style</th>
+        <th class='dateCreatedCol'>Date Created</th>
+        <!-- <th class='previewCol'>Preview</th>
+        <th class='editCol'>Edit</th>
+        <th class='addCol'>Add</th> -->
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      //unset the sessions for preview tasks in filter existing tasks from comments.php
+      if(isset($_SESSION["create"]))
+      unset($_SESSION["create"]);
+      if(isset($_SESSION["edit"]))
+      unset($_SESSION["edit"]);
 
-	if(isset($_GET["from"])){
-		$from = $_GET["from"];
-		$_SESSION["from"] = $from;
-	}
-	if(isset($_GET["testID"])){
-		$testID = $_GET["testID"];
-		$_SESSION["testID"] = $testID;
-	}
-	$from = "filterExistingTasks";
-	include_once 'filterTasks.php';
-  ?>
-  </tbody>
+      if(isset($_GET["from"])){
+        $from = $_GET["from"];
+        $_SESSION["from"] = $from;
+      }
+      if(isset($_GET["testID"])){
+        $testID = $_GET["testID"];
+        $_SESSION["testID"] = $testID;
+      }
+      $from = "filterExistingTasks";
+      include_once 'filterTasks.php';
+      ?>
+    </tbody>
   </div>
 </table>
 </div>
 <div class="row">
-	<div class="col s12">
-		<!--<a class="waves-effect waves-light btn #2196f3 blue right" id="cancelButton">Cancel</a>-->
-		<a class="waves-effect waves-light btn red cancelButton right" onClick="javascript:history.go(-1)">Cancel</a>
-	</div>
+  <div class="col s12">
+    <!--<a class="waves-effect waves-light btn #2196f3 blue right" id="cancelButton">Cancel</a>-->
+    <a class="waves-effect waves-light btn red cancelButton right" onClick="javascript:history.go(-1)">Cancel</a>
+  </div>
 </div>
 </div>
 <!--end body content-->
@@ -190,9 +213,6 @@ function checkDate(){
 .nav-wrapper > ul {
   margin-left: 220px;
 }
-.header{
-  margin-top: 30px;
-}
 .cancelButton{
   margin-top: 15px;
   width: 100px;
@@ -214,49 +234,19 @@ function checkDate(){
 td .btn{
   width: 100px;
 }
-/* tbody {
-  display:block;
-  height:300px;
-  overflow:auto;
+.input-field {
+  margin-top: 0rem;
 }
-thead, tbody tr {
-  display:table;
-  width:100%;
-  table-layout:fixed;
+.sortButton:hover, .editButton:hover, .addButton:hover, .previewButton:hover{
+  background-color: #FF8C18!important;
 }
-thead {
-  width: calc( 100% - 1em )
+.card-panel {
+    padding: 10px 24px;
+    margin: .5rem 0 1rem 0;
 }
-table {
-  width:100%;
-} */
-/*th{
-  text-align: center;
+.row {
+    margin-bottom: 0px;
 }
-.taskIdCol, .activityStyleCol,.dateCreatedCol, .previewCol, .editCol, .addCol{
-  text-align: center;
-}*/
-/* .taskIdCol{
-  width: 1%;
-}
-.instructionCol{
-  width: 30%;
-}
-.activityStyleCol{
-  width: 18%;
-}
-.dateCreatedCol{
-  width: 15%;
-}
-.previewCol{
-  width: 10%;
-}
-.editCol{
-  width: 5%;
-}
-.addCol{
-  width: 10%;
-}
-*/
+
 </style>
 </html>
